f64d8ea1527f35a110f821d0054e3a7f4cdfed8e
Sudhan Bhattarai <bhattaraiavinav400@gmail.com>
2024-05-13 19:17:30 +0545

    Picture can be sent over TCP connection and packets implementation successful

---
+++ prototypes/avinav/database.h
@@ -0,0 +1,58 @@
+#pragma once
+#include <iostream>
+#include <sqlite3.h>
+
+
+//Creates the database if not available
+
+
+static int createDB(const char* databasePath) {
+	sqlite3* DB;
+	int exit = sqlite3_open(databasePath, &DB);
+	sqlite3_close(DB);
+	return 0;
+}
+
+static int createTable(const char* databasePath) {
+	sqlite3* DB;
+	std::string sqlCommand = "Create TABLE IF NOT EXISTS MESSAGES("
+		"ID INTEGER PRIMARY KEY AUTOINCREMENT,"
+		"SENDER		TEXT NOT NULL,"
+		"MESSAGE	TEXT NOT NULL,"
+		"CHANNELID	INTEGER NOT NULL);";
+
+	try {
+		int exit = 0;
+		exit = sqlite3_open(databasePath, &DB);
+
+		char* messageError;
+		exit = sqlite3_exec(DB, sqlCommand.c_str(), NULL, 0, &messageError);
+
+		if (exit != SQLITE_OK) {
+			std::cerr << "Error Creating a table" << std::endl;
+			sqlite3_free(messageError);
+		}
+		sqlite3_close(DB);
+	}
+	catch (const std::exception& e) {
+		std::cerr << e.what() << std::endl;
+	}
+	return 0;
+}
+
+static int insertData(const char* databasePath, std::string clientID, std::string message, int channelID) {
+	sqlite3* DB;
+	char* messageError;
+	int exit = sqlite3_open(databasePath, &DB);
+
+	std::string sql = "INSERT INTO MESSAGES (SENDER, MESSAGE) VALUES('" + clientID + "','" + message + "'," + std::to_string(channelID) + ");";
+	std::cout << sql.c_str() << std::endl;
+
+	exit = sqlite3_exec(DB, sql.c_str(), NULL, 0, &messageError);
+
+	if (exit != SQLITE_OK) {
+		std::cerr << "Error Inserting data" << messageError << std::endl;
+		sqlite3_free(messageError);
+	}
+	return 0;
+}
